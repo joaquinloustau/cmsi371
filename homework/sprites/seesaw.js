eef5bc2fe4c1e0f337f2b177d528ae276a41e980
jloustau <jloustau@lion.lmu.edu>
2015-01-29 17:30:02 -0800

    Bug fixed in seesaw.js

--- homework/sprites/seesaw.js
+++ homework/sprites/seesaw.js
@@ -2,36 +2,43 @@
   window.LoustauSprites = window.LoustauSprites || {};
   window.LoustauSprites.Seesaw = (function () {
     var leverLength = 5;
-    var inclination = 0;
     var DEFAULT_X_FULCRUM = 0;
     var DEFAULT_Y_FULCRUM = 0;
     var DEFAULT_HEIGHT_FULCRUM = 20;
     var DEFAULT_LENGTH_LEVER = 250;
-    var DEFAULT_INCLINATION = 0
+    var DEFAULT_INCLINATION = 1;
 
-var drawSeesaw = function (ctx, options) {
-    ctx.linewidth = 1;
-    ctx.save();
-    ctx.translate(200,200);
-    var xFulcrum = options.xFulcrum || DEFAULT_X_FULCRUM;
-    var yFulcrum = options.yFulcrum || DEFAULT_Y_FULCRUM;
-    var heightFulcrum = options.heightFulcrum || DEFAULT_HEIGHT_FULCRUM;
-    var lengthLever = options.lengthLever || DEFAULT_LENGTH_LEVER;
-    var inclination = options.inclination || DEFAULT_INCLINATION;
-    var angle = Math.asin(heightFulcrum/(lengthLever/2));
-    var inclination = 0;
-    ctx.beginPath();
-    ctx.arc(xFulcrum,yFulcrum+heightFulcrum,heightFulcrum,0, Math.PI, true);
-    ctx.lineTo(xFulcrum+heightFulcrum,yFulcrum+heightFulcrum);
-    ctx.stroke();
-    //draw Lever
-    ctx.beginPath();
-    ctx.rotate(angle*inclination);
-    ctx.moveTo(xFulcrum - (lengthLever/2), yFulcrum);
-    ctx.lineTo(xFulcrum + (lengthLever/2), yFulcrum);
-    ctx.rotate(1*heightFulcrum/(lengthLever/2));
-    ctx.stroke();
-}
+    var drawSeesaw = function (ctx, options) {
+        ctx.linewidth = 1;
+        ctx.save();
+        var xFulcrum = options.xFulcrum || DEFAULT_X_FULCRUM;
+        var yFulcrum = options.yFulcrum || DEFAULT_Y_FULCRUM;
+        var heightFulcrum = options.heightFulcrum || DEFAULT_HEIGHT_FULCRUM;
+        var lengthLever = options.lengthLever || DEFAULT_LENGTH_LEVER;
+        var inclination = options.inclination || DEFAULT_INCLINATION;
+        var angle = Math.asin(heightFulcrum/(lengthLever/2));
+        var imageObj = new Image();
+       
+        //draw Fulcrum
+        ctx.beginPath();
+        ctx.arc(xFulcrum,yFulcrum+heightFulcrum,heightFulcrum,0, Math.PI, true);
+        ctx.lineTo(xFulcrum+heightFulcrum,yFulcrum+heightFulcrum);
+        ctx.fillStyle = "black";
+        ctx.fill();
+        ctx.stroke();
+        //draw Lever
+        ctx.beginPath();
+        ctx.rotate(angle*inclination);
+        ctx.moveTo(xFulcrum - (lengthLever/2), yFulcrum);
+        ctx.lineTo(xFulcrum + (lengthLever/2), yFulcrum);
+        ctx.lineTo(xFulcrum + (lengthLever/2), yFulcrum-10);
+        ctx.lineTo(xFulcrum - (lengthLever/2), yFulcrum-10);
+        ctx.lineTo(xFulcrum - (lengthLever/2), yFulcrum);
+        ctx.fillStyle = "brown";
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+    }
 
     return {draw: drawSeesaw};
   })();
