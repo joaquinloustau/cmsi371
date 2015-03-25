71129defa5bd4547dbd232bfafd68fc31d4e8403
jloustau <jloustau@lion.lmu.edu>
2015-03-24 20:52:19 -0700

    Got stuck while refactoring the code. Will focus on the other parts of the assignment and come back to the refactoring part.

--- homework/pipeline/hello-webgl-again.js
+++ homework/pipeline/hello-webgl-again.js
@@ -171,7 +171,7 @@
 
         {
             color: { r: 0.0, g: 0.5, b: 0.0 },
-            vertices: Shapes.toRawLineArray(Shapes.icosahedron()),
+            vertices: Shape.toRawLineArray(Shape.icosahedron()),
             mode: gl.LINES
         }
     ];
